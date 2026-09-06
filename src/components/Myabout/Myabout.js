e3f5b25396b10336b46b51226f429d16f51788fb
Dhasneem Beevi <122379925+Dhasneembeevi@users.noreply.github.com>
2026-09-06 15:22:58 +0530

    Updated Myabout.js

--- src/components/Myabout/Myabout.js
+++ src/components/Myabout/Myabout.js
@@ -179,7 +179,7 @@ const Myabout = () => {
             className="js"
           />
 
-          // <img src={js} alt="logo" className="js" />
+          {/*<img src={js} alt="logo" className="js" />*/}
           <img src={db} alt="logo" className="db" />
           <img src={exp} alt="logo" className="exp" />
           <img
