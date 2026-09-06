ef5a07a86145022fdb88074b8c34d979557e67ba
Dhasneem Beevi <122379925+Dhasneembeevi@users.noreply.github.com>
2026-09-06 14:40:06 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -34,10 +34,10 @@ const App = () => {
     return null;
   };
 
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-    localStorage.setItem("darkMode", !darkMode);
-  };
+  // const toggleDarkMode = () => {
+  //   setDarkMode(!darkMode);
+  //   localStorage.setItem("darkMode", !darkMode);
+  // };
 
   useEffect(() => {
     document.body.className = darkMode ? "dark" : "light";
